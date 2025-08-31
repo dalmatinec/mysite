a7acb3dd26a24193ec705e98ba3e3e398ac8a704
dalmatinec <temirbekov1602@gmail.com>
2025-08-31 17:21:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ const backToTop = document.querySelector('.back-to-top');
 if (backToTop) {
   window.addEventListener('scroll', () => {
     if (window.scrollY > 300) {
-      backToTop:classList.add('show');
+      backToTop.classList.add('show');
     } else {
       backToTop.classList.remove('show');
     }
